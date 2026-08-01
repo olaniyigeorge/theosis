0550e778f2b935450a8a9937d85065afee7077be
olaniyigeorge <olaniyigeorge77@gmail.com>
2026-08-01 23:55:56 +0100

    Update clean_clothes script

--- apps/api/clean_clothes.py
+++ apps/api/clean_clothes.py
@@ -1,4 +1,4 @@
-
+import time
 
 
 """
@@ -6,8 +6,7 @@ STATE = dirty clothes
 
 move the clothes to the laundry
 
-sort the clothes with colors 
-
+sort the clothes by colors
 
 pick the correct soap
 put water in the machine
@@ -16,27 +15,27 @@ get the clothes with brighter color
 dump them in the machine
 turn on the machine
 
-wait for 30 miniutes
+wait for 30 minutes
 
-turn off the machine 
+turn off the machine
 
 get the clothes out of the machine
 
 if the water is still clean
     use the same water to wash the colored clothes
 if not
-    put water in the machine(do line 11)
+    put water in the machine
     add soap
 
 dump the next set of clothes in the machine
 
 turn on the machine
 
-wait for another 30 minutes 
+wait for another 30 minutes
+
+turn off the machine
 
 drain the water
-
-in the same order
 
 rinse the first set of clothes
 
@@ -48,16 +47,105 @@ repeat the last step x times
 
 wring the clothes
 
-hang them on the lines 
+hang them on the lines
 
 END STATE = clean clothes
 """
 
-PSEUDOCODE =  ""
+
+dirty_clothes_basket = [
+    "black_shirt",
+    "white_short",
+    "blue_jean_short",
+    "white_singlet",
+    "red_jacket"
+]
+
+print("\nTaking my dirty clothes to the laundry")
+
+bright_dirty_clothes_group = []
+dull_dirty_clothes_group = []
+
+print("\nStart sorting by color")
+
+for cloth in dirty_clothes_basket:
+    if cloth.__contains__("white"):
+        bright_dirty_clothes_group.append(cloth)
+    else:
+        dull_dirty_clothes_group.append(cloth)
+
+print("I have finished sorting by color")
 
 
+soap = "Klin"
 
-clean = "white shirt"
+print(f"\nPicking {soap} soap")
+
+print("Pouring water into the machine")
+print(f"Adding {soap} soap")
+
+print("Putting the bright clothes in the machine")
+print("Turning on the washing machine")
+
+time.sleep(5)   # waiting for the machine to wash for 30 minutes
+
+print("Turning off the machine")
+print("Removing the bright clothes from the machine")
 
 
-print(clean.__contains__("white"))
+water_is_clean = False
+
+if water_is_clean:
+    print("\nWater is still clean, reusing it")
+else:
+    print("\nDraining the dirty water")
+    print("Pouring clean water into the machine")
+    print(f"Adding {soap} soap")
+
+
+print("Putting the colored clothes in the machine")
+print("Turning on the washing machine")
+
+time.sleep(5)   # waiting for another 30 minutes
+
+print("Turning off the machine")
+
+print("Draining the water")
+
+
+print("\nRinsing the bright clothes")
+
+for rinse in range(3):
+    print(f"Rinse {rinse + 1}")
+
+print("Finished rinsing the bright clothes")
+
+
+print("\nRinsing the colored clothes")
+
+for rinse in range(3):
+    print(f"Rinse {rinse + 1}")
+
+print("Finished rinsing the colored clothes")
+
+
+print("\nWringing all the clothes")
+
+clean_clothes = []
+
+for cloth in bright_dirty_clothes_group:
+    print(f"Wringing {cloth}")
+    clean_clothes.append(cloth)
+
+for cloth in dull_dirty_clothes_group:
+    print(f"Wringing {cloth}")
+    clean_clothes.append(cloth)
+
+
+print("\nHanging the clothes on the line")
+
+for cloth in clean_clothes:
+    print(f"Hanging {cloth}")
+
+
+print("\nEND STATE = clean clothes")
